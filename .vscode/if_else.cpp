7aa4db37c29353d07c3c39ba4406e9b3542ad354
siddhant2153 <siddhantkumar@noreply.com>
2023-07-28 22:54:33 +0530

    commit 1

---
+++ .vscode/if_else.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   /*int a,b;
+    cout<<" enter the value of a"<<endl;
+    cin>>a;
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+
+   if (a>0)
+    {
+        cout<<"a is postive:"<<a<<endl;
+    }
+    else{
+        cout<<"a is neagtive:"<<a<<endl;
+    }
+
+    if (a>b)
+    {
+
+
+        cout<<"a is greater then b"<<endl;
+    }
+    else
+    {
+        cout<<"b is greater then a"<<endl;
+
+    }*/
+
+ /* int a;
+  cout<<"enter the value of a" <<endl;
+  cin>>a;
+  
+  if (a>0)
+  {
+    cout<<"a is positive"<<endl;
+  }
+  else if (a<0)
+    {
+    cout<<"a is negative"<<endl;
+        
+    }
+    else{
+        cout<<"a is zero"<<endl;
+    
+  
+  }
+*/
+
+
+
+char c;
+cout<<"enter the value of c"<<endl;
+cin>>c;
+
+if ()
+{
+    /* code */
+}
+
+
+
+
+  
+} 
---
+++ data_type.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    int a= 123;
+    cout<< a <<endl;
+    char b='a';
+    cout<<b <<endl;
+    bool c=true;
+    cout<<c<<endl;
+    float d=1.2;
+    cout<<d<<endl;
+    double e=1.23;
+    cout<<e<<endl;
+    int size= sizeof(e);
+    cout<< "size of e is:"<<size<<endl;
+
+int a='a';
+cout<<a<<endl;
+char b=98;
+cout<<b<<endl;
+char c=122345;
+cout<<c;
+
+unsigned int a=-234;
+cout<<a<<endl;
+
+float b= 2.0/5;
+cout<<b<<endl;
+*/
+int a=2;
+int b=3;
+ bool c=(a==b);
+cout<<c<<endl;
+ bool d=(a<b);
+cout<<d<<endl;
+ bool e=(a>b);
+cout<<e<<endl;
+ bool f=(a!=b);
+cout<<f<<endl;
+
+}
